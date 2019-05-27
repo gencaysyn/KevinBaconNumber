fdc2a1ba8eb92212049585e269e787a350733f40
Gencay <gencaysyn@gmail.com>
2019-05-27 09:29:38 +0300

    Update

--- Main.c
+++ Main.c
@@ -3,7 +3,7 @@
 #include<string.h>
 
 #include"hash.h"
-
+#include"queue.h"
 #define MAX_FILM 15000
 #define MAX_ARTIST 180000
 struct info{
@@ -26,33 +26,39 @@ void  freeList  (char* ,node**);
 Info readFile(char* );
 Info initialize(int , int);
 void deInitialize(Info*, int, int);
-int isContains(char**,int,char*);
+//int isContains(char**,int,char*);
+void bfs(Info, char[100], char[100]);
+int getArtId(Info , char [100]);
 
 node *filmRoot[MAX_FILM] ;
 node *artistRoot[MAX_ARTIST];
 
 int main(){
-	
+
 	int i;
 	for(i = 0; i < MAX_ARTIST; i++){
 		if(i < MAX_FILM)
 			filmRoot[i] = NULL;
 		artistRoot[i] = NULL;
 	}
+	
 	Info info = readFile("input-mpaa.txt");
+	
+	
 	//printTable(info.artists);
+	
 	/*for(i = 0; i < MAX_ARTIST; i++){
 		if(i < MAX_FILM && strlen(info.films[i]) > 0){
-			printf("film %s: ",info.films[i]);
+			printf("film[%d] %s: ",i,info.films[i]);
 			printList(filmRoot[i]);
 		}
 		if(artistRoot[i] != NULL){
-			printf("artist %s : ", info.artists.node[artistRoot[i][0].val].val);	
+			printf("artist[%d] %s : ",i, info.artists.node[artistRoot[i][0].val].val);	
 			printList(artistRoot[i]);	
 		}
 		
 	}*/
-
+	bfs(info, "Pitt, Brad","Pitt, Brad");
 	return(0);
 }
 
@@ -137,7 +143,7 @@ Info initialize(int maxFilm, int maxArtist){
 		if(i<maxFilm){
 			info.films[i] = (char*) calloc(100, sizeof(char));
 			if(info.films[i] == NULL)
-				printf("MallocFilms Error!");
+				printf("CallocFilms Error!");
 		}
 	}
 	return info;
@@ -211,5 +217,77 @@ Info readFile(char* fileName){
 	return info;
 }
 
+int getArtId(Info info, char word[100]){
+	return info.artists.node[search(info.artists,word)].idx;
+}
+
+void bfs(Info info, char source[100], char dest[100]){
+	int visFilms[MAX_FILM];
+	int visArtist[MAX_ARTIST];
+	struct queue *qA = createQueue();
+	struct queue *qF = createQueue();
+	int element;
+	int i;
+	printf("s:%s, d:%s\n",source,dest);
+	if(!strcmp(source,dest)){
+		printf("%s\n",source);
+		printf("Bacon Number is 0\n");
+		return;
+	}
+	
+	for(i=0;i<MAX_ARTIST;i++){
+		if(i<MAX_FILM){
+			visFilms[i] = 0;
+		}
+		visArtist[i] = 0;
+	}
+	
+	int s = getArtId(info,source);
+	int d = getArtId(info,dest);
+	visArtist[s] = 1;
+	
+	node *iter = artistRoot[s]->next;
+	
+	while(iter != NULL){		
+		enqueue(qF,iter->val);
+		visFilms[iter->val] = 1;
+		iter = iter->next;
+	}
+	
+	
+	printf("\ns:%d, d:%d\n",s,d);
+	int counter = 1;
+	while(1){
+		while(!isEmpty(qF)){
+			element = dequeue(qF);
+			iter = filmRoot[element]->next;
+			while(iter!= NULL){
+				if(visArtist[info.artists.node[iter->val].idx] == 0){
+					enqueue(qA,info.artists.node[iter->val].idx);
+					visArtist[info.artists.node[iter->val].idx] = counter;
+				}
+
+				if(info.artists.node[iter->val].idx == d){
+					printf("Bulduk!");
+					printf("counter:%d",counter);
+					exit(1);
+				}
+				iter = iter->next;
+			}
+		}
+		while(!isEmpty(qA)){
+			element = dequeue(qA);
+			iter = artistRoot[element]->next;
+			while(iter != NULL){
+				if(visFilms[iter->val] == 0){
+					enqueue(qF,iter->val);
+					visFilms[iter->val] = counter;
+				}
+				iter = iter->next;
+			}
+		}
+		counter++;
+	}
+}
 
 
---
+++ queue.h
@@ -0,0 +1,69 @@
+#ifndef MY_QUEUQ_H
+#define MY_QUEUQ_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 500000
+
+struct queue {
+    int items[SIZE];
+    int front;
+    int rear;
+};
+
+struct queue* createQueue() {
+    struct queue* q = malloc(sizeof(struct queue));
+    q->front = -1;
+    q->rear = -1;
+    return q;
+}
+
+int isEmpty(struct queue* q) {
+    if(q->rear == -1) 
+        return 1;
+    else 
+        return 0;
+}
+
+void enqueue(struct queue* q, int value){
+    if(q->rear == SIZE-1)
+        printf("\nQueue is Full!!");
+    else {
+        if(q->front == -1)
+            q->front = 0;
+        q->rear++;
+        q->items[q->rear] = value;
+    }
+}
+
+int dequeue(struct queue* q){
+    int item;
+    if(isEmpty(q)){
+        printf("Queue is empty");
+        item = -1;
+    }
+    else{
+        item = q->items[q->front];
+        q->front++;
+        if(q->front > q->rear){
+            q->front = q->rear = -1;
+        }
+    }
+    return item;
+}
+
+void printQueue(struct queue *q) {
+    int i = q->front;
+
+    if(isEmpty(q)) {
+        printf("Queue is empty");
+    } else {
+        printf("Queue contains \n");
+        for(i = q->front; i < q->rear + 1; i++) {
+                printf("%d ", q->items[i]);
+        }
+    }  
+	printf("\n");  
+}
+
+#endif 
